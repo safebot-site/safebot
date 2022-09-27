c5d862596bce602daafbf9345e132418040531ea
Lucas Bovolini <lbovolini94@gmail.com>
2022-09-27 14:42:29 -0300

    adiciona conversao de cnpj filial para cnpj matriz

--- src/main.py
+++ src/main.py
@@ -47,13 +47,64 @@ async def validate_email(email: str):
         return False
     return True
 
+# calcula o CNPJ da matriz a partir de outro CNPJ
+def calculate_cnpj_matriz(site_cnpj):
+    cnpj = list(site_cnpj[:12])
+    cnpj[8] = '0'
+    cnpj[9] = '0'
+    cnpj[10] = '0'
+    cnpj[11] = '1'
+    cnpj = ''.join(cnpj)
+
+    if (site_cnpj.endswith("0001", 0, 12)):
+        return site_cnpj
+    reverse_cnpj = list(map(int, cnpj[::-1]))
+
+    pesos = [2, 3, 4, 5, 6, 7, 8, 9, 2, 3, 4, 5]
+    for i in range(12):
+        reverse_cnpj[i] = reverse_cnpj[i] * pesos[i]
+
+    soma = 0
+    for i in range(12):
+        soma += reverse_cnpj[i]
+    
+    primeiro_digito_verificador = 11 - (soma % 11)
+
+    if (primeiro_digito_verificador >= 10):
+        primeiro_digito_verificador = '0'
+    else:
+        primeiro_digito_verificador = str(primeiro_digito_verificador)
+    
+    cnpj_matriz = list(cnpj)
+    cnpj_matriz.append(primeiro_digito_verificador)
+    reverse_cnpj = list(map(int, ''.join(cnpj_matriz)[::-1]))
+    pesos = [2, 3, 4, 5, 6, 7, 8, 9, 2, 3, 4, 5, 6]
+    for i in range(13):
+        reverse_cnpj[i] = reverse_cnpj[i] * pesos[i]
+
+    soma = 0
+    for i in range(13):
+        soma += reverse_cnpj[i]
+    
+    segundo_digito_verificador = 11 - (soma % 11)
+
+    if (segundo_digito_verificador >= 10):
+        segundo_digito_verificador = '0'
+    else:
+        segundo_digito_verificador = str(segundo_digito_verificador)
+    
+    cnpj_matriz.append(segundo_digito_verificador)
+
+    return ''.join(cnpj_matriz)
+
 # validar o CNPJ do dono do site com o CNPJ da empresa 
 async def validate_cnpj(site_url, site_cnpj):
     domain_url = site_url[7:]
     response = await http_client.get(f"https://rdap.registro.br/domain/{domain_url}")
     cnpj = response.json()["entities"][0]["handle"]
+    cnpj_matriz = calculate_cnpj_matriz(site_cnpj)
 
-    return site_cnpj==cnpj
+    return cnpj_matriz==cnpj
 
 @app.post("/verify")
 async def root(site: Site):
