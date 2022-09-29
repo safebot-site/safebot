9244f8cc637bc040f8018856e9d945509173eb32
Lucas Bovolini <lbovolini94@gmail.com>
2022-09-29 14:08:43 -0300

    adiciona testes unitarios da conversao de cnpj filial para cnpj raiz

--- test/test_main.py
+++ test/test_main.py
@@ -1,2 +1,18 @@
-def test_foo_bar():
-   assert True
+from src.main import calculate_cnpj_matriz
+
+# verifica se dado um CNPJ matriz o mesmo CNPJ matriz é retornado
+def test_cnpj_matriz():
+   cnpj_matriz_input = "45543915000181"
+
+   cnpj_matriz_result = calculate_cnpj_matriz(cnpj_matriz_input)
+
+   assert cnpj_matriz_result == cnpj_matriz_input
+
+# verifica se dado um CNPJ filial o CNPJ matriz é retornado
+def test_cnpj_filial():
+   cnpj_filial_input = "45543915084695"
+   cnpj_matriz_esperado = "45543915000181"
+
+   cnpj_matriz_result = calculate_cnpj_matriz(cnpj_filial_input)
+
+   assert cnpj_matriz_result == cnpj_matriz_esperado
